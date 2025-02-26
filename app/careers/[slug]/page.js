45bea1f245e7a871f3876c05b1c3d0cb49a99a6d
mani-sagravia <mani@sagravia.com>
2025-02-26 21:36:44 +0530

    Updated Careers slug page

---
+++ app/careers/[slug]/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+ export default function Careers(){
+    return(
+        <div>
+            <h1 className="text-2xl text-black py-20">welcome to the jobs!</h1>
+        </div>
+    )
+ }
