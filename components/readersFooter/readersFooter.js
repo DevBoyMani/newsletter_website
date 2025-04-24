09a275e3f42ed8195cd4204d546594c063fc9a9e
mani-sagravia <mani@sagravia.com>
2025-04-24 19:58:50 +0530

    updated moving animation for the footer cards

--- components/readersFooter/readersFooter.js
+++ components/readersFooter/readersFooter.js
@@ -23,6 +23,7 @@ const feedbackData = [
     feedback:
       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.",
   },
+  
 ];
 
 const socialMediaIcons=[
