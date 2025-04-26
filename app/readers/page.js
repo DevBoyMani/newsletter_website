6796583446e7bf8e5218b8e389888296199489ff
mani-sagravia <mani@sagravia.com>
2025-04-26 22:03:47 +0530

    Updated footer web view

--- app/readers/page.js
+++ app/readers/page.js
@@ -26,8 +26,9 @@ export default function ReadersPage(){
 
             <div className="block lg:hidden">
               <ReadersMobileView/>
-            </div>
+              
             <ReadersFooter/>
+            </div>
         </>
     )
 }
