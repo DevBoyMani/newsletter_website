5173f86d4b6296bb2d7cb85ee3870e51b8c3631b
Elango-Sagravia <elango@sagravia.com>
2025-12-23 16:57:32 +0530

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-   },
-};
+  },
+} satisfies NextConfig;
 
 export default nextConfig;
